4386040c7439bf5d9d5269239faba1cd48dd43b1
Paozin Do Mta YT <150871304+paozinkkj@users.noreply.github.com>
2026-06-10 22:56:16 -0300

    Update script URL in Duper.lua

--- Duper.lua
+++ Duper.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://roshield.fun/raw/3832d7d3-38db-4dce-8ff4-0e74a7de2dac"))()
+loadstring(game:HttpGet("https://roshield.fun/raw/f2fee3d4-84ce-48da-8cda-b1c5cd718d7f"))()
